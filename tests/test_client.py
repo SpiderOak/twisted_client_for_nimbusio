c650a5f072e7a1a5e6137f44553d4ee880c9b146
dougfort <dougfort@spideroak.com>
2013-01-31 12:07:31 -0500

    Full archive including receiving version from response

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,7 +15,11 @@ from twisted.web.client import FileBodyProducer
 
 from twisted_client_for_nimbusio.archiver import archive
 
+import motoboto
 from motoboto.identity import load_identity_from_file
+
+class SetupError(Exception):
+    pass
 
 _program_description = "Test twisted_client_for_nimbusio"
 
@@ -38,22 +42,38 @@ def _parse_commandline():
                         help="Path to (motoboto) nimbus.io identity file")
     return parser.parse_args()
 
+def _setup_test(args):
+    if args.identity_file is None:
+        raise SetupError("you must specify a nimbus.io identity file")
+    identity = load_identity_from_file(args.identity_file)
+    if args.identity_file is None:
+        raise SetupError("unable to load nimbus.io identity from {0}".format(
+                         args.identity_file))
+
+    s3_connection = motoboto.connect_s3(identity)
+    bucket = s3_connection.create_unique_bucket()
+    s3_connection.close()
+
+    return identity, bucket.name
+
 def _error(failure):
-    log.msg("Failure %s" % (failure, ), logLevel=logging.ERROR)
+    log.msg("Failure %s" % (failure.getErrorMessage(), ), 
+            logLevel=logging.ERROR)
 
 def _shutdown(ignored):
     log.msg('shutting down', logLevel=logging.DEBUG)
     reactor.stop()
 
 def _archive_result(result):
-    log.msg("result = %s" % (result,), logLevel=logging.INFO)
+    log.msg("archive successful: version = %s" % (result["version_identifier"],), 
+            logLevel=logging.INFO)
 
-def _start_test(identity):
-    log.msg("starting user_name = %s" % (identity.user_name, ), 
+def _start_test(identity, collection_name):
+    log.msg("starting user_name = %s collection = %s" % (identity.user_name, 
+                                                         collection_name), 
             logLevel=logging.DEBUG)
 
-    collection_name = "foop"
-    key = "glort"
+    key = "twisted_client_key"
     bodyProducer = FileBodyProducer(StringIO("test body"))
 
     deferred = archive(identity, collection_name, key, bodyProducer)
@@ -70,16 +90,13 @@ if __name__ == "__main__":
     python_log = logging.getLogger("__main__")
 
     args = _parse_commandline()
-    if args.identity_file is None:
-        python_log.error("you must specify a nimbus.io identity file")
-        sys.exit(1)
-    identity = load_identity_from_file(args.identity_file)
-    if args.identity_file is None:
-        python_log.error("unable to load nimbus.io identity from {0}".format(
-                  args.identity_file))
+    try:
+        identity, collection_name = _setup_test(args)
+    except Exception:
+        python_log.exception("_setup_test")
         sys.exit(1)
 
     observer = log.PythonLoggingObserver()
     observer.start()
-    reactor.callLater(0, _start_test, identity)
+    reactor.callLater(0, _start_test, identity, collection_name)
     reactor.run()
--- twisted_client_for_nimbusio/archiver.py
+++ twisted_client_for_nimbusio/archiver.py
@@ -5,21 +5,38 @@ archiver.py
 archive data stream to nimbus.io
 """
 import httplib
+import json
 import logging
 import os
 import urllib
 
 from twisted.python import log
 
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
+from twisted.internet.protocol import Protocol
 
-from twisted.web.client import Agent
+from twisted.web.client import Agent, ResponseDone
 from twisted.web.http_headers import Headers
 
 from lumberyard.http_util import compute_collection_hostname, \
         compute_uri as compute_uri_path, \
         current_timestamp, \
         compute_authentication_string
+
+class ReceiveResponseProtocol(Protocol):
+    def __init__(self, deferred):
+        self._deferred = deferred
+        self._buffer = ""
+
+    def dataReceived(self, bytes):
+        self._buffer = "".join([self._buffer, bytes])
+
+    def connectionLost(self, failure):
+        if failure.check(ResponseDone):
+            result = json.loads(self._buffer)
+            self._deferred.callback(result)
+        else:
+            self.deferred.errback(failure)
 
 _connection_timeout = float(os.environ.get("NIMBUSIO_CONNECTION_TIMEOUT", 
                                            "360.0"))
@@ -47,18 +64,26 @@ def _compute_headers(identity, method, path):
 
     return headers
 
-def _push_response(response):
-    successful = True
+def _push_response(response, final_deferred):
     if response.code != httplib.OK:            
-        log.msg("(%d) %s" % (response.code, response.phrase), 
+        error_message = "Invalid HTTP Status: (%s) %s" % (respose.code, 
+                                                          response.phrase)
+        log.msg("_push_response %s" % (error_message, ), 
                 logLevel=logging.ERROR)
-        successful = False
-    return successful        
+        final_deferred.errback(error_message)
+    else:
+        response.deliverBody(ReceiveResponseProtocol(final_deferred))
+
+def _push_error(failure, final_deferred):
+    log.msg("_push_error %s" % (failure.getErrorMessage(), ), 
+            logLevel=logging.ERROR)
+    final_deferred.errback(failure)
 
 def archive(identity, collection_name, key, bodyProducer):
     """
     return a deferred that fires with the response to an HTTP(S) PUSH request
     """
+    final_deferred = defer.Deferred()
     method = "POST"
     path = compute_uri_path("data", key)
     uri = _compute_uri(collection_name, path)
@@ -66,8 +91,9 @@ def archive(identity, collection_name, key, bodyProducer):
 
     agent = Agent(reactor)
     log.msg("requesting '%r" % (uri, ), logLevel=logging.DEBUG)
-    deferred = agent.request(method, uri, headers, bodyProducer)
+    request_deferred = agent.request(method, uri, headers, bodyProducer)
 
-    deferred.addCallback(_push_response)
+    request_deferred.addCallback(_push_response, final_deferred)
+    request_deferred.addErrback(_push_error, final_deferred)
 
-    return deferred
+    return final_deferred
